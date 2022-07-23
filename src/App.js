df4ce20af406b25c2af37871b1d8a996cb2518de
Christopher Mullins <mullinschristopher2479@gmail.com>
2022-07-23 12:53:20 +0100

    changed menue to a navBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/Main';
+import Quiz from './components/Quizly'
+import Scores from './components/Scores'
+import Genre from './components/Genre';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -12,7 +15,7 @@ import {
   Route
 } from "react-router-dom";
 import Info from './components/Info';
-import Create from './components/create';
+import Create from './components/Scores';
 import logo from './assets/Logo.png'
 
 class App extends Component {
@@ -25,12 +28,18 @@ class App extends Component {
             <Navbar.Brand href="/"><img src={logo} alt="Logo"></img></Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/">Main</Nav.Link>
+              <Nav.Link href="/quiz">Start</Nav.Link>
+              <Nav.Link href="/scores">Scores</Nav.Link>
+              <Nav.Link href="/genre">Genre</Nav.Link>
               <Nav.Link href="/info">Info</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
         <Switch>
           <Route path="/" exact><Main /></Route>
+          <Route path="/quiz" exact><Quiz /></Route>
+          <Route path="/scores" exact><Scores /></Route>
+          <Route path="/genre" exact><Genre /></Route>
           <Route path="/info"><Info></Info></Route>
         </Switch>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -9,33 +9,25 @@ import Genre from './Genre';
 import { useHistory } from 'react-router-dom';
 
 function Main (){
-    const history = useHistory();
-
-    const navigateToQuiz = () => history.push('/quizly');
-    const navigateToLeader = () => history.push('/leaderboard');
-    const navigateToGenre = () => history.push('/genre');
-
         return (
             <div className='background'>
                 <img height={200} width={200} src={logo} alt="Logo"></img>
                 <h1 className='title'>Welcome To Quizly</h1>
-                <div>
+
+                {/* <div>
                         <button className='startButton' onClick={navigateToQuiz}>Start Quiz</button>
                         <p></p>
                         <button className='mainMenueButton' onClick={navigateToLeader}>Leaderboard</button>
                         <p></p>
                         <button className='mainMenueButton' onClick={navigateToGenre}>Genre</button>
-                </div>
+                </div> */}
+
                 <footer className='footer'>
                     <p>Arthor: Christopher Mullins</p>
                     <p>Quizly PPIT</p>
                 </footer>
-
-                <Router>
-                <Switch>
-                    <Route path="/genre"><Genre></Genre></Route>
-                </Switch>
-                </Router>
+                
+                
             </div>
         );
     }
